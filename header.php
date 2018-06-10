e49ef1c9a02156e80e34dd1704b36b6fe8ea3b7e
huynhvothinh <huynhvothinh@gmail.com>
2018-06-10 07:52:02 +0700

    update header

--- header.php
+++ header.php
@@ -40,7 +40,7 @@ require_once('include.php');
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="index.php"><?php t_('My Profile');?></a>
+        <a class="nav-link" href="/index.php"><?php t_('My Profile');?></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/views/user/users.php"><?php t_('Users');?></a>
